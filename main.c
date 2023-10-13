610ae575690a5c89972a06ca2dd54b1fb69d20b4
mikromalekula1100 <max.kalutskij17@gmail.com>
2023-10-13 17:36:26 +0300

    final

--- main.c
+++ main.c
@@ -1,56 +1,48 @@
 #include "./include/generating_cards.h"
 
 
-
-
 int main() {
     
     pthread_mutex_t mutex;
     pthread_mutex_init(&mutex, NULL);
 
-    struct Data data;
-    data.sum[0] = 0;
-    data.sum[1] = 0;
-    data.mutex = mutex;
+    struct Data transmitted_data;
+    transmitted_data.sum[0] = 0;
+    transmitted_data.sum[1] = 0;
+    transmitted_data.mutex = mutex;
 
     int thread_count;
     long round_count;
 
-    printf("Введите количество потоков, используемое для вычислений.");
+    printf("Введите количество потоков, используемое для вычислений.\n");
     scanf("%d", &thread_count);
 
-    printf("Введите количество раундов.");
+    printf("Введите количество раундов.\n");
     scanf("%ld", &round_count);
-
-    data.round_count = round_count / thread_count;
-    
-
-    pthread_t pthread_id[thread_count];
-
 
     struct timeval start_time, end_time;
     long seconds, microseconds;
     double elapsed_time;
-    gettimeofday(&start_time, NULL); 
+    gettimeofday(&start_time, NULL);
+
+    transmitted_data.round_count = round_count / thread_count;
     
-
-
+    pthread_t pthread_id[thread_count];
+    
     for (int i = 0; i < thread_count; ++i)
-    {   
-        pthread_create(&pthread_id[i], NULL, generating_cards, (void*)(&data));
-    }
-
+        pthread_create(&pthread_id[i], NULL, generating_cards, (void*)(&transmitted_data));
+    
     for (int i = 0; i < thread_count; ++i)
-    {
         pthread_join(pthread_id[i], NULL);
-    }
     
-    printf("%f \n", (double)data.sum[0] / data.sum[1]); 
+    printf("%f \n", (double)transmitted_data.sum[0] / transmitted_data.sum[1]); 
 
     gettimeofday(&end_time, NULL); 
     seconds = end_time.tv_sec - start_time.tv_sec;
     microseconds = end_time.tv_usec - start_time.tv_usec;
     elapsed_time = seconds + (microseconds / 1000000.0);
+    
     printf("Время выполнения программы: %f секунд\n", elapsed_time);
+    
     return 0;
 }
--- src/generating_cards.c
+++ src/generating_cards.c
@@ -1,31 +1,31 @@
 #include "../include/generating_cards.h"
 
-const int count_cards = 52;
-void* generating_cards(void* args) {
-    
-    printf("%ld \n", (*((struct Data*)(args))).round_count);
-    
 
-    long round_count = (*((struct Data*)(args))).round_count;
+const int count_cards = 52;
+
+void* generating_cards(void* args) {    
 
     pthread_mutex_lock(&(*((struct Data*)(args))).mutex);
-    unsigned int seed = time(NULL);
+    unsigned int individual_time = time(NULL);
     pthread_mutex_unlock(&(*((struct Data*)(args))).mutex);
 
-    long first_value, second_value, number_matches = 0, number_failures = 0;
+    long first_value, second_value, number_matches = 0, number_failures = 0, round_count = (*((struct Data*)(args))).round_count;
 
-    for (long i = 0; i < round_count; i++)
+    for (long i = 0; i < round_count; ++i)
     {   
-        first_value = rand_r(&seed) % count_cards + 1;
-        while((second_value = (rand_r(&seed) % count_cards + 1)) == first_value){continue;}
+        first_value = rand_r(&individual_time) % count_cards + 1;
         
-        if((abs(first_value - second_value) == 1) && ((first_value / 4) == (second_value / 4))){
+        while((second_value = (rand_r(&individual_time) % count_cards + 1)) == first_value)
+            continue;
+        
+        if((abs(first_value - second_value) == 1) && ((first_value / 4) == (second_value / 4)))//  To do:чекни правильность
             number_matches++;
-        } 
-        else{
+        
+        else
             number_failures++;
-        } 
+        
     }
+
     pthread_mutex_lock(&(*((struct Data*)(args))).mutex);
             (*((struct Data*)(args))).sum[1] += number_failures;
             (*((struct Data*)(args))).sum[0] += number_matches;
